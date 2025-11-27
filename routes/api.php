2db11ff972683455d415ff9ae151c475bd43121e
Duynguyen08 <zoronami444@gmail.com>
2025-11-27 20:50:30 +0700

    Clean up remaining conflict markers in routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,7 @@ Route::post('/chat', [ChatController::class, 'chat']);
 
 Route::get('/booked-seats/{showtimeId}', [BookingController::class, 'getBookedSeats']);
 Route::get('/showtime-seats/{showtimeId}', [BookingController::class, 'getShowtimeSeats']);
-<<<<<<< HEAD
-Route::get('/ticket/{id}', [BookingController::class, 'getTicket']);
-=======
 Route::get('/ticket/{id}', [BookingController::class, 'getTicket']);
 
 Route::post('/showtimes/{suatChieu}/select-seats', [BookingController::class, 'selectSeats'])
     ->name('api.select.seats');
->>>>>>> origin/hoanganh
